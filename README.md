441d62206502d5b5a43fee557834583f7750feca
justDenzil <vikash.denzil@gmail.com>
2021-11-17 08:24:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tejknt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tejknt)
---
+++ index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Menu, Button } from 'antd';
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+
+class App extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+      <div style={{ width: 256 }}>
+        <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+          {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+        </Button>
+        <Menu
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          inlineCollapsed={this.state.collapsed}
+        >
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Dashboard
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            People
+          </Menu.Item>
+          <Menu.Item key="3" icon={<ContainerOutlined />}>
+            Project
+          </Menu.Item>
+          <Menu.Item key="4" icon={<ContainerOutlined />}>
+            Skills
+          </Menu.Item>
+          <Menu.Item key="4" icon={<ContainerOutlined />}>
+            Deep View
+          </Menu.Item>
+          
+        </Menu>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
